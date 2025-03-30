6f7eab8afda3629ad42f3f8238ada28264b509f3
Ippat <nathan.ly.study@gmail.com>
2025-03-31 10:05:45 +1100

    initial files for api

---
+++ postcode-backend/src/main/java/io/nology/postcode_backend/PostcodeBackendApplication.java
@@ -0,0 +1,13 @@
+package io.nology.postcode_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostcodeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostcodeBackendApplication.class, args);
+	}
+
+}
---
+++ postcode-backend/src/test/java/io/nology/postcode_backend/PostcodeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.nology.postcode_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostcodeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
